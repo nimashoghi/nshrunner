e7ad6074ad07f8aad8ef4753038dc6e707235af8
Nima Shoghi <nimashoghi@gmail.com>
2024-07-23 17:15:46 -0400

    refactor: Add RunnerConfigDict to Runner

--- src/nshrunner/__init__.py
+++ src/nshrunner/__init__.py
@@ -2,6 +2,7 @@ from ._runner import Config as Config
 from ._runner import PythonLoggingConfig as PythonLoggingConfig
 from ._runner import RunInfo as RunInfo
 from ._runner import Runner as Runner
+from ._runner import RunnerConfigDict as RunnerConfigDict
 from ._runner import SeedConfig as SeedConfig
 from ._runner import SnapshotConfig as SnapshotConfig
 from ._runner import runner as runner
