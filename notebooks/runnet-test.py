c085933a854d8aa95c76e2cc3b09a9f025ea059f
Nima Shoghi <nimashoghi@gmail.com>
2024-07-23 16:42:58 -0400

    chore: Add SLURM job submission for distributed training

--- notebooks/runnet-test.py
+++ notebooks/runnet-test.py
@@ -16,3 +16,16 @@ list(runner.local(runs))
 
 # %%
 runner.session(runs, snapshot=False, pause_before_exit=True)
+
+# %%
+runner.submit_slurm(
+    runs,
+    {
+        "partition": "learnaccel",
+        "nodes": 4,
+        "ntasks_per_node": 8,  # Change this to limit # of GPUs
+        "gpus_per_task": 1,
+        "cpus_per_task": 1,
+    },
+    snapshot=False,
+)
