6e268b5dacf0e125cc3a801f74f0560e44782e1d
Nima Shoghi <nimashoghi@gmail.com>
2024-07-24 11:38:39 -0400

    Use nshutils for logging setup Update version

--- src/nshrunner/_logging.py
+++ src/nshrunner/_logging.py
@@ -1,8 +1,8 @@
-import logging
 from pathlib import Path
 from typing import Literal, TypeAlias
 
 import nshconfig as C
+import nshutils
 
 LogLevel: TypeAlias = Literal[
     "CRITICAL", "FATAL", "ERROR", "WARN", "WARNING", "INFO", "DEBUG"
@@ -42,45 +42,11 @@ class PythonLoggingConfig(C.Config):
 
 
 def init_python_logging(config: PythonLoggingConfig):
-    if config.lovely_tensors:
-        try:
-            import lovely_tensors  # type: ignore
-
-            lovely_tensors.monkey_patch()
-        except ImportError:
-            logging.warning(
-                "Failed to import `lovely_tensors`. Ignoring pretty PyTorch tensor formatting"
-            )
-
-    if config.lovely_numpy:
-        try:
-            import lovely_numpy  # type: ignore
-
-            lovely_numpy.set_config(repr=lovely_numpy.lovely)
-        except ImportError:
-            logging.warning(
-                "Failed to import `lovely_numpy`. Ignoring pretty numpy array formatting"
-            )
-
-    log_handlers: list[logging.Handler] = []
-    if config.log_save_dir is not None:
-        log_file = config.log_save_dir / "python_log.log"
-        log_file.touch(exist_ok=True)
-        log_handlers.append(logging.FileHandler(log_file))
-
-    if config.rich:
-        try:
-            from rich.logging import RichHandler  # type: ignore
-
-            log_handlers.append(RichHandler(rich_tracebacks=config.rich_tracebacks))
-        except ImportError:
-            logging.warning(
-                "Failed to import rich. Falling back to default Python logging."
-            )
-
-    logging.basicConfig(
-        level=config.log_level,
-        format="%(message)s",
-        datefmt="[%X]",
-        handlers=log_handlers,
+    nshutils.init_python_logging(
+        lovely_tensors=config.lovely_tensors,
+        lovely_numpy=config.lovely_numpy,
+        rich=config.rich,
+        rich_tracebacks=config.rich_tracebacks,
+        log_level=config.log_level,
+        log_save_dir=config.log_save_dir,
     )
