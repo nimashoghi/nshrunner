246acfdd6b25584a9c57cafbf97636850a256b20
Nima Shoghi <nimashoghi@gmail.com>
2024-07-26 15:24:20 -0400

    Remove unused _find_git_dir function

--- src/nshrunner/_util/git.py
+++ src/nshrunner/_util/git.py
@@ -1,16 +1,6 @@
 from pathlib import Path
 
 
-def _find_git_dir(self):
-    current_dir = Path.cwd()
-    while current_dir != current_dir.parent:
-        git_dir = current_dir / ".git"
-        if git_dir.is_dir():
-            return git_dir
-        current_dir = current_dir.parent
-    return None
-
-
 def _gitignored_dir(path: Path, *, create: bool = True) -> Path:
     if create:
         path.mkdir(exist_ok=True, parents=True)
