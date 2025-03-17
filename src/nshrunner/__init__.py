ed0d76ca8e676358b51dc8080d31fecbe3923580
Nima Shoghi <nimashoghi@gmail.com>
2025-03-17 20:26:45 +0000

    Feature: Export Submission class from module

--- src/nshrunner/__init__.py
+++ src/nshrunner/__init__.py
@@ -10,6 +10,7 @@ from . import session as session
 from ._runner import Config as Config
 from ._runner import Config as RunnerConfig
 from ._runner import Runner as Runner
+from ._submit._util import Submission as Submission
 from .session import Session as Session
 from .wrapper_fns import run_local as run_local
 from .wrapper_fns import submit_screen as submit_screen
